a0e0677063b7152eec1d7e420986a9a1c7b0f118
Guilbert21 <guilbertalexis534@gmail.com>
2023-03-02 19:19:58 +0400

    first commit

--- week_3/day-3/ExerciseXP/Exercise4/main.js
+++ week_3/day-3/ExerciseXP/Exercise4/main.js
@@ -1,9 +1,14 @@
 const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
 
-// ["Lea123"]=true
-// ["Princesse45"]=true
-// ["cat&doglovers"]=true
-// ["helooo@000"]=true
+const numUsers = users.length;
 
-if (users.length===1);
-
+if (numUsers === 0) {
+  console.log("no one is online");
+} else if (numUsers === 1) {
+  console.log(`${users[0]} is online`);
+} else if (numUsers === 2) {
+  console.log(`${users[0]} and ${users[1]} are online`);
+} else {
+  const numAdditionalUsers = numUsers - 2;
+  console.log(`${users[0]}, ${users[1]} and ${numAdditionalUsers} more are online`);
+}
